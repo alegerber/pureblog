f6d82a315c21ad432d84f6d4b1fb2120d463a72a
Alexander Gerber <alex.gerber90@gmail.com>
2019-11-29 21:35:11 +0100

    add jsonResponse

---
+++ Core/JsonResponse.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core;
+
+class JsonResponse
+{
+    /**
+     * @var mixed $content
+     */
+    private $content;
+
+    /**
+     * @var int $statusCode
+     */
+    private $statusCode;
+
+    /**
+     * @var string $header
+     */
+    private $header;
+
+    /**
+     * JsonResponse constructor.
+     * @param string $content
+     * @param int $statusCode
+     * @param string $header
+     */
+    public function __construct($content, int $statusCode = 200, string $header = '')
+    {
+        $this->statusCode = $statusCode;
+        $this->content    = $content;
+        $this->header     = $header;
+    }
+
+    public function __toString()
+    {
+        \header(sprintf('HTTP/1.1 %s OK', $this->statusCode) . $this->header);
+
+        return json_encode(
+            $this->content,
+            JSON_THROW_ON_ERROR
+        );
+    }
+}
