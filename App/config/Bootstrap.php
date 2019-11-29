71861563f098881517cb1cb7923f2a5298dc9b45
Alexander Gerber <alex.gerber90@gmail.com>
2019-11-29 23:54:50 +0100

    add basic

--- App/config/Bootstrap.php
+++ App/config/Bootstrap.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 spl_autoload_register(function ($className) {
-    include __DIR__ . '../../' . preg_replace('/\\\\/', '/', $className). '.php';
+    include __DIR__ . '/../../' . preg_replace('/\\\\/', '/', $className). '.php';
 });
 
 require_once 'Routes.php';
