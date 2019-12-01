583faefce7091aa743d468beee79098c0df07288
Alexander Gerber <alex.gerber90@gmail.com>
2019-12-01 18:27:53 +0100

    add blog

--- public/js/View.js
+++ public/js/View.js
@@ -2,10 +2,22 @@
 
 class View {
     constructor() {
-        const ajax = new Ajax('/views/base.html');
-        ajax.get(function (xhttp) {
+        this.ajax = new Ajax(false);
+        this.ajax.get('/views/base.html',function (xhttp) {
             document.createElement("body");
             document.body.innerHTML = xhttp.responseText;
+        });
+    }
+
+    blog() {
+        const store = new Store();
+        store.createStore('blogItem', localStorage);
+        const ajaxHtml = new Ajax('/views/blog/post.html');
+        ajaxHtml.get(function (xhttp) {
+        });
+        const ajaxJson = new Ajax('/post/');
+        ajax.get(function (xhttp) {
+            document.body.main.innerHTML = xhttp.responseText;
         });
     }
 }
