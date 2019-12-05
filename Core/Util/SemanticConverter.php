e1da8393fb6d38a28385c41be038fe1675dde158
Alexander Gerber <alex.gerber90@gmail.com>
2019-12-05 21:29:05 +0100

    add SemanticConverter

--- Core/Util/SemanticConverter.php
+++ Core/Util/SemanticConverter.php
@@ -6,13 +6,58 @@ namespace Core\Util;
 
 class SemanticConverter
 {
-    public static function camelCaseToSnakeCase($input): string
+    public static function camelCaseToSnakeCase(string $input): string
     {
-        preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
-        $ret = $matches[0];
-        foreach ($ret as &$match) {
-        $match = $match === strtoupper($match) ? strtolower($match) : lcfirst($match);
+        $matches = [];
+        $result = '';
+
+        preg_match('/[A-Z][a-z]*/', $input, $matches);
+
+        $matches[0] = preg_match('/^[a-z]*/', $input);
+
+        foreach ($matches as $match) {
+            $result .= strtolower($match) . '_';
         }
-        return implode('_', $ret);
+
+        return substr($result,0,-1);
+    }
+
+    public static function snakeCaseToCamelCase(string $input): string
+    {
+        $result = '';
+
+        foreach (explode('_', $input) as $item){
+            $result .= ucfirst($item);
+        }
+
+        return lcfirst($result);
+    }
+
+    public static function pascalCaseToSnakeCase(string $input): string
+    {
+        $matches = [];
+        $result = '';
+
+        preg_match('/[A-Z][a-z]*/', $input, $matches);
+
+        unset($matches[0]);
+
+
+        foreach ($matches as $match) {
+            $result .= strtolower($match) . '_';
+        }
+
+        return substr($result,0,-1);
+    }
+
+    public static function snakeCaseToPascalCase(string $input): string
+    {
+        $result = '';
+
+        foreach (explode('_', $input) as $item){
+            $result .= ucfirst($item);
+        }
+
+        return $result;
     }
 }
