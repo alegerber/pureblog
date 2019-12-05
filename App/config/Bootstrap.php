f10adec12abe447681b60e712d18dc6b997f13c6
Alexander Gerber <alex.gerber90@gmail.com>
2019-12-05 22:02:48 +0100

    add env config

--- App/config/Bootstrap.php
+++ App/config/Bootstrap.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+$config = [];
+
+$config = array_merge($config, include('./env/config.php'));
+$config = array_merge($config, include('./env/config_dev.php'));
+
 spl_autoload_register(function ($className) {
     include __DIR__ . '/../../' . preg_replace('/\\\\/', '/', $className). '.php';
 });
