7f4cfb15903060578a7a66107340a7c24bd4ec9e
Alexander Gerber <alex.gerber90@gmail.com>
2019-12-07 17:37:17 +0100

    config config

--- App/config/Bootstrap.php
+++ App/config/Bootstrap.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 $config = [];
 
-$config = array_merge($config, include('./env/config.php'));
-$config = array_merge($config, include('./env/config_dev.php'));
+$config = array_merge($config, include(__DIR__ . '/env/config.php'));
+$config = array_merge($config, include(__DIR__ . '/env/config_dev.php'));
 
 spl_autoload_register(function ($className) {
     include __DIR__ . '/../../' . preg_replace('/\\\\/', '/', $className). '.php';
--- Tests/Runner.php
+++ Tests/Runner.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 $config = [];
 
-//$config = array_merge($config, include('./../App/config/env/config.php'));
-//$config = array_merge($config, include('./../App/config/env/config_test.php'));
-
+$config = array_merge($config, include(__DIR__ . '/../App/config/env/config.php'));
+$config = array_merge($config, include(__DIR__ . '/../App/config/env/config_test.php'));
 
 spl_autoload_register(function ($className) {
     include __DIR__ . '/../' . preg_replace('/\\\\/', '/', $className). '.php';
