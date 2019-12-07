d2ca485fb28bc7720caa2caceec0a47b7ffda10a
Alexander Gerber <alex.gerber90@gmail.com>
2019-12-07 18:03:01 +0100

    optimize

--- public/js/app/View.js
+++ public/js/app/View.js
@@ -4,7 +4,7 @@ class View {
     constructor() {
         this.ajax = new Ajax(false);
         this.ajax.get('/views/base.html',function (xhttp) {
-            document.createElement("body");
+            document.body = document.createElement("body");
             document.body.innerHTML = xhttp.responseText;
         });
     }
@@ -12,9 +12,6 @@ class View {
     blog() {
         const store = new Store();
         store.createStore('blogItem', localStorage);
-        this.ajax.get('/post', function (xhttp) {
-            store.addItem('blogJson', xhttp.responseText)
-        });
         this.ajax.get('/views/blog/post.html', function (xhttp) {
             document.body.main.innerHTML = xhttp.responseText;
         });
